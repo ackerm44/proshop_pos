6ac809810a7fec610470da0ad052a05202e80f7c
ackerm44 <ackerm44@gmail.com>
2018-04-23 17:25:02 -0400

    Generated Product model, Department and Product table data

--- db/migrate/20180423203403_create_departments.rb
+++ db/migrate/20180423203403_create_departments.rb
@@ -1,7 +1,8 @@
 class CreateDepartments < ActiveRecord::Migration[5.2]
   def change
     create_table :departments do |t|
-
+      t.string :name
+      t.string :description
       t.timestamps
     end
   end
---
+++ db/migrate/20180423210122_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.decimal :tax_percent, :precision => 3, :scale => 2, :default => 0.00
+      t.decimal :price, :precision => 6, :scale => 2
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
